16cfaf8db0a4411d69b4fb58e30ce3b6422b2d6b
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2022-01-24 17:11:32 +0000

    [Start]3-課題

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+  def new
+  end
+  
+  def index
+    @books = Book.all
+  end
+  
+  def create
+　  book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)  
+  end
+
+  def show
+    @book = Book.find(params[:id])  
+  end
+
+  def edit
+    @book = Book.find(params[:id])  
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
